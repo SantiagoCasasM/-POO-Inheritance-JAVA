14e205497c28c9195531b11b5f25f73e83f21843
Santiago <santiago28oc@hotmail.com>
2020-05-10 14:39:01 -0500

    POO-Herencia Finalizado

---
+++ ProgramacionOrientadaObjetos/src/programacionorientadaobjetos/jcClase1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionorientadaobjetos;
+
+/**
+ *
+ * @author Santiago
+ */
+public class jcClase1 {
+    public int Suma (int N1, int N2){
+        return N1 + N2;
+    }
+
+private double Sumar (int N1, double N2){
+    return N1 + N2;
+    }
+}
---
+++ ProgramacionOrientadaObjetos/src/programacionorientadaobjetos/jcClase2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionorientadaobjetos;
+
+/**
+ *
+ * @author Santiago
+ */
+public class jcClase2 extends jcClase1{
+    
+    public double Sumar(double N1, double N2){
+        return N1 + N2;
+    }
+    
+}
